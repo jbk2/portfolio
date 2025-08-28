fb84ab4503c26974cde1a8a84940bfca0efb1109
jbk2 <james@humanbenefits.com>
2025-08-28 18:06:48 +0200

    Create a Content component to hosue personal blogging

---
+++ src/components/Content.jsx
@@ -0,0 +1,14 @@
+export default function Content() {
+
+  return(
+    <div className='my-18 page-width'>
+      <hr className="mx-auto border-[var(--color-border-primary)] my-14"></hr>
+      <div className="px-4 sm:px-10 lg:pl-[88px] xl:pl-40 my-6">
+        <h2 className="text-lg self-center font-extrabold font-inter tracking-wider text-blue-900
+        underline underline-offset-4 decoration-4 decoration-emerald-200 ">MY CONTENT</h2>
+        <a href="">Dev.to article 1</a>
+        <a href="">Dev.to article 2</a>
+      </div>
+    </div>
+  )
+}
