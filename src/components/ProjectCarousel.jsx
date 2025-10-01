64d567c8eec3465a3b900c888f72fcbb30076d2f
jbk2 <james@humanbenefits.com>
2025-10-01 16:59:52 +0200

    Begin Projects description re-layout

--- src/components/ProjectCarousel.jsx
+++ src/components/ProjectCarousel.jsx
@@ -5,9 +5,17 @@ import waldo from '/src/assets/images/project-images/waldo.png';
 import forhm from '/src/assets/images/project-images/forhm.png';
 import ubuntu from '/src/assets/images/project-images/ubuntu.png';
 import { useState, useRef, useEffect } from 'react';
+import  railsLogo from '../assets/images/technology-images/rails.svg';
+import  hotwireLogoLight from '../assets/images/technology-images/hotwire-light.svg';
+import  hotwireLogoDark from '../assets/images/technology-images/hotwire-dark.svg';
+import  reactLogo from '../assets/images/technology-images/react.svg';
+import  rspecLogo from '../assets/images/technology-images/rspec.svg';
+import  viteLogo from '../assets/images/technology-images/vite.svg';
+import  tailwindLogo from '../assets/images/technology-images/tailwindcss.svg';
 
 export default function ProjectCarousel() {
-
+  const dataTheme = () => document.documentElement.getAttribute('data-theme');
+  const [theme, setTheme] = useState(() => dataTheme());
   const projectsRef = useRef(null);
   const [projectsInView, setProjectsInView] = useState(false);
   
@@ -35,6 +43,13 @@ export default function ProjectCarousel() {
     return () => observer.disconnect();
   }, [])
 
+  useEffect(() => {
+    const onThemeChangeEvent = (e) => setTheme(e.detail)
+    window.addEventListener('themeChange', onThemeChangeEvent);
+    
+    return() => window.removeEventListener('themeChange', onThemeChangeEvent);
+  }, [])
+
   return(
     <div className='my-14 bg-transparent'>
       <div className="page-width pl-40 sm:pl-10 lg:pl-[88px] xl:pl-40">
@@ -46,44 +61,80 @@ export default function ProjectCarousel() {
       <div ref={projectsRef} className='flex justify-center px-10 py-2 bg-transparent'>
         <div className={`carousel carousel-center max-w-fit bg-transparent rounded-box
           ${projectsInView ? `animate-projects-in` : ``}`}>
-          <div id="slide1" className="carousel-item flex flex-col font-inter">
+          <div id="slide1" className="carousel-item flex flex-col w-108">
             <div className='rotate-1 p-1'>
               <a href="https://fakebook.bibble.com">
-                <img src={fakebook} className="rounded h-[280px] drop-shadow-lg
+                <img src={fakebook} className="mx-auto rounded h-[280px] drop-shadow-lg
                   transition ease-in-out hover:scale-[101%]"/>
               </a>
             </div>
             <div className="mt-5 ml-6 text-[var(--color-text-secondary)]">
-              <h1 className="font-bold text-[var(--color-text-header-secondary)]
-                [html[data-theme=dark]_&]:text-[var(--color-text-header-primary)]">
-                Facebook clone SPA type app
-              </h1>
-              <ul className="list-inside list-disc font-light text-sm">
-                <li className="">Rails</li>
-                <li>Hotwire</li>
-                <li>Websockets</li>
-              </ul>
+              <div className='flex flex-col items-center'>
+                <h1 className="flex font-bold text-[var(--color-text-header-secondary)]
+                  [html[data-theme=dark]_&]:text-[var(--color-text-header-primary)]">
+                  Facebook clone SPA type app
+                </h1>
+                <div className='flex items-center gap-2 text-[var(--color-text-header-secondary)]'>
+                  <img src={railsLogo} alt="rails logo" className='size-10 -mt-[13px]'/>
+                  <p className='text-xs -mt-[12px]'>+</p>
+                  <img src={rspecLogo} alt="rspec logo" className='size-4 -mt-[13px]'/>
+                  <p className='text-xs -mt-[12px]'>+</p>
+                  <img src={ theme == 'dark' ? hotwireLogoDark : hotwireLogoLight}
+                    alt="hotwire logo" className='size-10 -mt-[7px]'/>
+                </div>
+              </div>
+              <div className='grid grid-cols-2 gap-3'>
+                <ul className="font-light text-sm">
+                  <li className="[&::marker]:content-['•'] [&::marker]:text-[var(--color-teal-dark)]">&nbsp;Complex forms & assoc'ns</li>
+                  <li className="[&::marker]:content-['•'] [&::marker]:text-[var(--color-teal-dark)]">&nbsp;ActiveStorage</li>
+                  <li className="[&::marker]:content-['•'] [&::marker]:text-[var(--color-teal-dark)]">&nbsp;Jobs, Mailers</li>
+                </ul>
+                <ul className="font-light text-sm">
+                  <li className="[&::marker]:content-['•'] [&::marker]:text-[var(--color-teal-dark)]">&nbsp;Turbo, Stimulus</li>
+                  <li className="[&::marker]:content-['•'] [&::marker]:text-[var(--color-teal-dark)]">&nbsp;ActionCable</li>
+                  <li className="[&::marker]:content-['•'] [&::marker]:text-[var(--color-teal-dark)]">&nbsp;Full unit & system tests</li>
+                </ul>
+              </div>
             </div>
           </div>
-          <div id="slide2" className="carousel-item flex flex-col">
+          <div id="slide2" className="carousel-item flex flex-col w-108">
             <div className='-rotate-1 p-1'>
               <a href="https://waldo.bibble.com">
-                <img src={waldo} className="rounded h-[280px] drop-shadow-lg
+                <img src={waldo} className="mx-auto rounded h-[280px] drop-shadow-lg
                   transition ease-in-out hover:scale-[101%]"/>
               </a>
             </div>
             <div className="mt-5 ml-6 text-[var(--color-text-secondary)]">
-              <h1 className="font-bold text-[var(--color-text-header-secondary)]
-                [html[data-theme=dark]_&]:text-[var(--color-text-header-primary)]">
-                Wheres Waldo toy game app
-              </h1>
-              <ul className="list-inside list-disc font-light text-sm">
-                <li className="">React</li>
-                <li>Rails API</li>
-              </ul>
+              <div className='flex flex-col items-center'>
+                <h1 className="font-bold text-[var(--color-text-header-secondary)]
+                  [html[data-theme=dark]_&]:text-[var(--color-text-header-primary)]">
+                  Wheres Waldo toy game app
+                </h1>
+                <div className='flex items-center gap-2 mb-2 text-[var(--color-text-header-secondary)]'>
+                  <img src={reactLogo} alt="react logo" className='size-5 -mt-[4px]'/>
+                  <p className='text-xs -mt-[3px]'>+</p>
+                  <img src={viteLogo} alt="vite logo" className='size-4 -mt-[7px]'/>
+                  <p className='text-xs -mt-[3px]'>+</p>
+                  <img src={railsLogo} alt="rails logo" className='size-10 -mt-[7px]'/>
+                  <p className='text-xs -mt-[3px]'>+</p>
+                  <img src={tailwindLogo} alt="tailwind logo" className='size-5 -mt-[7px]'/>
+                </div>
+              </div>
+              <div className='grid grid-cols-2 gap-3'>
+                <ul className="font-light text-sm">
+                  <li className="[&::marker]:content-['•'] [&::marker]:text-[var(--color-teal-dark)]">&nbsp;React Router, Context API</li>
+                  <li className="[&::marker]:content-['•'] [&::marker]:text-[var(--color-teal-dark)]">&nbsp;callback|memo|ref|nav|params</li>
+                  <li className="[&::marker]:content-['•'] [&::marker]:text-[var(--color-teal-dark)]">&nbsp;Custom hooks</li>
+                </ul>
+                <ul className="font-light text-sm">
+                  <li className="[&::marker]:content-['•'] [&::marker]:text-[var(--color-teal-dark)]">&nbsp;Rails API + Postgres</li>
+                  <li className="[&::marker]:content-['•'] [&::marker]:text-[var(--color-teal-dark)]">&nbsp;Rails 8 auth served via React</li>
+                  <li className="[&::marker]:content-['•'] [&::marker]:text-[var(--color-teal-dark)]">&nbsp;Unit and integration tested</li>
+                </ul>
+              </div>
             </div>
           </div>
-          <div id="slide3" className="carousel-item flex flex-col">
+          <div id="slide3" className="carousel-item flex flex-col w-108">
             <div className='rotate-1 p-1'>
               <a href="https://replit.com/@jbk1/Chess">
                 <img src={chess} className="h-[280px] rounded drop-shadow-lg
@@ -101,7 +152,7 @@ export default function ProjectCarousel() {
               </ul>
             </div>
           </div>
-          <div id="slide4" className="carousel-item flex flex-col">
+          <div id="slide4" className="carousel-item flex flex-col w-108">
             <div className="-rotate-1 p-1">
               <a href="https://forhm.com/">
                 <img src={forhm} className="rounded h-[280px] drop-shadow-lg
@@ -119,7 +170,7 @@ export default function ProjectCarousel() {
               </ul>
             </div>
           </div>
-          <div id="slide5" className="carousel-item flex flex-col">
+          <div id="slide5" className="carousel-item flex flex-col w-108">
             <div className='rotate-1 p-1'>
               <a href="https://ubuntu.bibble.com">
                 <img src={ubuntu} className="rounded h-[280px] drop-shadow-lg
