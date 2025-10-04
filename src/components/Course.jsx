4deecef4287c4ba509f5fc8fe70f2f94178945eb
jbk2 <james@humanbenefits.com>
2025-10-04 13:29:08 +0200

    Update Course with line clamp 4 and overflow on hover

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,7 +1,7 @@
 export default function Course({key, title, progress, image, link, description}) {
 
   return(
-    <div key={key} className="card w-70 h-80 shadow-lg overflow-hidden bg-[var(--color-surface-card)]
+    <div key={key} className="card w-70 h-80 shadow-lg bg-[var(--color-surface-card)]
       border border-[var(--color-border-primary)]">
       <figure className="w-34 overflow-hidden mx-auto rounded-none">
         <a href={link} target="_blank" rel="noopener">
@@ -13,7 +13,10 @@ export default function Course({key, title, progress, image, link, description})
       <div className="card-body pt-4">
         <h2 className="card-title text-[var(--color-text-header-secondary)]
         [html[data-theme=dark]_&]:text-[var(--color-text-header-primary)]">{title}</h2>
-        <p className="text-[var(--color-text-secondary)] font-light">{description}</p>
+        <p className="text-[var(--color-text-secondary)] font-light whitespace-pre-line
+          line-clamp-4 hover:line-clamp-none hover:max-h-24 hover:overflow-y-auto">
+          {description}
+        </p>
         {/* <div className="card-actions justify-end">
           <button className="btn btn-primary">Buy Now</button>
           </div> */}
