32a627ac68458f397737c20c123fd39535634120
jbk2 <james@humanbenefits.com>
2025-10-07 18:20:37 +0200

    Layout adjustments to Introduction H2s, & space chars to 1st H2

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -2,13 +2,13 @@ import { BulletArrow } from "./icons/Icons"
 
 export default function Introduction() {  
   return(
-    <div className="page-width px-4 sm:px-10 my-14">
+    <div className="page-width px-4 sm:px-10 my-8 md:my-14">
       <hr className="section-hr"></hr>
       
       {/* Summary stats and intro bullet sections */}
       <div className="xl:ml-0 mt-6">
         {/* Summary stats section */}
-        <section className="font-semibold text-sm mb-9">
+        <section className="font-semibold text-sm mb-4 md:mb-9">
           <div className="flex justify-between md:justify-around gap-8 p-6
             text-[var(--color-text-header-secondary)]">
             <div className="flex flex-col md:items-center">
@@ -16,9 +16,9 @@ export default function Introduction() {
                 <span className="font-extrabold italic text-lg">11</span>
                 <span className="font-black text-xs">&nbsp;YRS</span>
               </div>
-              <p className="my-1 w-32 sm:w-fit">Coding since
-                <a href="https://makers.tech/learn/curriculum">&nbsp;Makers</a>
-                &nbsp;bootcamp
+              <p className="my-1 w-32 md:w-fit">Coding&nbsp;since&#32;
+                <a href="https://makers.tech/learn/curriculum">Makers</a>
+                &#32;bootcamp
               </p>
             </div>
             <div className="flex flex-col md:items-center">
@@ -27,14 +27,14 @@ export default function Introduction() {
                 <span className="font-extrabold italic text-sm diagonal-fractions">1/2</span>
                 <span className="font-black text-xs">&nbsp;YRS</span>
               </div>
-              <p className="my-1">Full time coding</p>
+              <p className="my-1 w-24 md:w-fit">Full time coding</p>
             </div>
             <div className="flex flex-col md:items-center">
               <div className="underline underline-offset-4 decoration-4 decoration-[var(--color-decoration-emerald)]">
                 <span className="font-extrabold italic text-lg ">15</span>
                 <span className="font-black text-xs">&nbsp;YRS</span>
               </div>
-              <p className="my-1">Digital product mgmt</p>
+              <p className="my-1 w-32 md:w-fit">Digital product mgmt</p>
             </div>
           </div>
         </section>
@@ -76,11 +76,11 @@ export default function Introduction() {
         <section className="flex w-full gap-4 sm:gap-14 justify-center xl:items-end xl:-mt-18" > {/*  gap-4 sm:gap-10 md:gap-14  */}
           <a href="https://github.com/jbk2" className="btn w-[120px] sm:w-[136px] rounded-lg border-none
             bg-[image:var(--btn-gradient-primary-darktolight)] text-[var(--btn-light-text)] hover:opacity-90
-            hover:scale-99" title="My Github Profile">
+            hover:scale-99 text-nowrap" title="My Github Profile">
             Github profile
           </a>
           <a href="mailto:james@bibble.com" className="btn w-[120px] sm:w-[136px] rounded-lg border-none
-            bg-[var(--color-btn-primary)] text-[var(--btn-dark-text)] hover:opacity-90 hover:scale-99"
+            bg-[var(--color-btn-primary)] text-[var(--btn-dark-text)] hover:opacity-90 hover:scale-99 text-nowrap"
             title="Email Me">
             Contact me
           </a>
