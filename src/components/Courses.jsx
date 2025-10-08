84a517875863906f575fc63aa6cd4c82f0b9ac56
jbk2 <james@humanbenefits.com>
2025-10-08 14:58:53 +0200

    Moving Courses back to single col on small screens

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -37,10 +37,8 @@ export default function Courses() {
           </ul>
         </section> */}
       </div>
-      <section ref={coursesRef} className='grid gap-6 mt-10 mx-auto px-4 sm:px-10
-        max-w-[2000px] justify-center
-        [grid-template-columns:repeat(2,18rem)]
-        md:[grid-template-columns:repeat(auto-fit,minmax(18rem,18rem))]
+      <section ref={coursesRef} className='grid justify-center mt-10 max-w-[2000px] gap-6
+        [grid-template-columns:repeat(auto-fit,minmax(18rem,18rem))]
         translate-y-[var(--scroll-shift,0px)]'>
         {
           courses.map(course => (
