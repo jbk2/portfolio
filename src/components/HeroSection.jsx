e2d3decd55a64b80dd4913e5930f807f78866eab
jbk2 <james@humanbenefits.com>
2025-10-16 16:00:59 +0200

    Adjust NavLinks responsive top & right values

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -68,7 +68,7 @@ export default function HeroSection() {
       </div>
 
       {/* Col-3 */}
-      <div className="absolute right-1 top-2 md:right-3 md:top-3 xl:right-8 xl:top-6">
+      <div className="absolute right-3 top-2 md:right-5 lg:top-3 xl:right-8 xl:top-5">
         <NavLinks />
       </div>
     </div>
